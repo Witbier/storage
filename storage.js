29cf81832b53333a826ce87398ae2376cda284f3
Ayush Srivastava <ayushs.1992@gmail.com>
2020-05-02 19:26:18 +0530

    Fixed default config.

--- storage.js
+++ storage.js
@@ -1,28 +1,46 @@
-if(!window) {
-    throw new Error("storage works only in browser that support local and Sesssion Storage.");
-}
-
-/**
- * 
- * @param {Object} config 
- */
-function Storage(config = {type = localStorage, async = false}) {
-
-    if(!config.type && (config.type === localStorage || config.type === sessionStorage)) {
-        throw new TypeError("config type should be localStorage or sessionStorage.");
+if (!window) {
+    throw new Error(
+      "storage works only in browser that support local and Sesssion Storage."
+    );
+  }
+  
+  /**
+   *
+   * @param {Object} config
+   */
+  function Storage(config) {
+    let newConfig = Object.assign(
+      {},
+      { type: localStorage, async: false },
+      config
+    );
+    if (
+      !newConfig.type &&
+      (newConfig.type === localStorage || newConfig.type === sessionStorage)
+    ) {
+      throw new TypeError(
+        "config type should be localStorage or sessionStorage."
+      );
     }
-    
+  
+    const { type, async } = newConfig;
+  
+    function wrapPromise(value) {
+      return async ? Promise.resolve(value) : value;
+    }
+  
     this.get = function(key) {
-        
-    }
-
+      return wrapPromise(JSON.parse(type.getItem(key)));
+    };
+  
     this.set = function(key, value) {
-
-    }
-
+      return wrapPromise(type.setItem(key, JSON.stringify(value)));
+    };
+  
     this.delete = function(key) {
-
-    }
-}
-
-export default Storage;
+      return wrapPromise(delete type[key]);
+    };
+  }
+  
+  export default Storage;
+  
