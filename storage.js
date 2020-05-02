43ace57f74c841bee30321df9e545a3814eadb79
Ayush Srivastava <ayushs.1992@gmail.com>
2020-05-02 20:00:37 +0530

    optimized code.

--- storage.js
+++ storage.js
@@ -1,46 +1,27 @@
-if (!window) {
-    throw new Error(
-      "storage works only in browser that support local and Sesssion Storage."
-    );
+if (!window)
+  throw new Error("storage works only in browser that support local and Sesssion Storage.");
+
+/**
+ *
+ * @param {Object} config
+ */
+function Storage(config) {
+  const newConfig = Object.assign({}, { type: localStorage, async: false }, config);
+
+  if (!newConfig.type &&(newConfig.type === localStorage || newConfig.type === sessionStorage))
+    throw new TypeError("config type should be localStorage or sessionStorage.");
+
+  const { type, async } = newConfig;
+
+  function _wrapPromise(value) {
+    return async ? Promise.resolve(value) : value;
   }
-  
-  /**
-   *
-   * @param {Object} config
-   */
-  function Storage(config) {
-    let newConfig = Object.assign(
-      {},
-      { type: localStorage, async: false },
-      config
-    );
-    if (
-      !newConfig.type &&
-      (newConfig.type === localStorage || newConfig.type === sessionStorage)
-    ) {
-      throw new TypeError(
-        "config type should be localStorage or sessionStorage."
-      );
-    }
-  
-    const { type, async } = newConfig;
-  
-    function wrapPromise(value) {
-      return async ? Promise.resolve(value) : value;
-    }
-  
-    this.get = function(key) {
-      return wrapPromise(JSON.parse(type.getItem(key)));
-    };
-  
-    this.set = function(key, value) {
-      return wrapPromise(type.setItem(key, JSON.stringify(value)));
-    };
-  
-    this.delete = function(key) {
-      return wrapPromise(delete type[key]);
-    };
-  }
-  
-  export default Storage;
-  
+
+  this.get = key => _wrapPromise(JSON.parse(type.getItem(key)));
+
+  this.set = (key, value) => _wrapPromise(type.setItem(key, JSON.stringify(value)));
+
+  this.delete = key => _wrapPromise(delete type[key]);
+}
+
+export default Storage;
