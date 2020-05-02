8ea748c2052f2b6ee55fca847dfb8f5ca676d7db
Ayush Srivastava <ayushs.1992@gmail.com>
2020-05-02 18:56:58 +0530

    Added package.json and storage file.

---
+++ storage.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @param {Object} config 
+ */
+function Storage(config = {type = localStorage, async = false}) {
+
+    if(!config.type && (config.type === localStorage || config.type === sessionStorage)) {
+        throw new TypeError("config type should be localStorage or sessionStorage.");
+    }
+    
+    this.get = function(key) {
+        
+    }
+
+    this.set = function(key, value) {
+
+    }
+
+    this.delete = function(key) {
+
+    }
+}
+
+export default Storage;
